ca8c459b7790feec8d6b0327ab69e262579c50e6
kocopo <o.kocka@okocka.cz>
2018-08-07 22:01:25 +0200

    Default Changelist

---
+++ app/presenters/BasePresenter.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette\Application\UI;
+use Tracy\Debugger;
+
+
+abstract class BasePresenter extends UI\Presenter
+{
+
+    public function __construct()
+    {
+        if(!isset($_SESSION['theme']) || !isset($_SESSION['textColor']) || !isset($_SESSION['bgcolor'])){
+            $_SESSION['theme'] = 'bg-light';
+            $_SESSION['textColor'] = 'text-dark';
+            $_SESSION['bgcolor'] = 'white';
+        }
+
+    }
+
+
+    protected function createComponentChangeTheme()
+{
+    $form = new UI\Form;
+    $form->addSubmit('change', 'Změnit téma');
+    $form->onSuccess[] = [$this, 'changeThemeSucceeded'];
+    return $form;
+}
+
+public function changeThemeSucceeded(UI\Form $form, $values){
+        Debugger::barDump($_SESSION);
+        Debugger::barDump($_SESSION['theme'] === 'bg-light');
+        if($_SESSION['theme'] === 'bg-light'){
+            $_SESSION['theme'] = 'bg-dark';
+            $_SESSION['textColor'] = 'text-light';
+            $_SESSION['bgcolor'] = '#1b1e21';
+        }else{
+            $_SESSION['theme'] = 'bg-light';
+            $_SESSION['textColor'] = 'text-dark';
+            $_SESSION['bgcolor'] = 'white';
+        }
+
+    $this->flashMessage("Téma bylo změněno", "info");
+    $this->redirect('this');
+}
+
+}
--- app/presenters/ShitPresenter.php
+++ app/presenters/ShitPresenter.php
@@ -2,14 +2,14 @@
 
 namespace App\Presenters;
 
-use Nette;
-
-class ShitPresenter extends Nette\Application\UI\Presenter
+class ShitPresenter extends BasePresenter
 {
 
 
     public function renderDefault(){
+
         $this->flashMessage("Pro přístup na tuto stránku se musíte nejprve přihlásit.", "success");
     }
+
 
 }
