9c1781bda33188fd23c88af8f61e47c7ff9824ab
kocopo <o.kocka@okocka.cz>
2019-01-14 15:37:55 +0100

    Zařátek guide psaní

---
+++ app/presenters/ChampsOfLightPresenter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Presenters;
+
+class ChampsOfLightPresenter extends BasePresenter
+{
+
+
+    public function __construct()
+    {
+    }
+
+    public function renderDefault(){
+
+    }
+
+
+}
--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -2,80 +2,74 @@
 
 namespace App\Presenters;
 
-use Nette;
-use Nette\Application\UI;
-use Tracy\Debugger;
-
 class HomepagePresenter extends BasePresenter
 {
-    /** @var Nette\Database\Context */
-    private $database;
 
-    public function __construct(Nette\Database\Context $database)
+
+    public function __construct()
     {
-        $this->database = $database;
     }
 
     public function renderDefault(){
 
     }
 
-    public function createComponentThumb()
-    {
+//    public function createComponentThumb()
+//    {
+//
+//     $form = new UI\Form();
+//     $form->addText('url');
+//     $form->addSubmit('submit','sub');
+//     $form->onSuccess[] = [$this ,'thumbSucceeded'];
+//     return $form;
+//    }
 
-     $form = new UI\Form();
-     $form->addText('url');
-     $form->addSubmit('submit','sub');
-     $form->onSuccess[] = [$this ,'thumbSucceeded'];
-     return $form;
-    }
-
-    public function thumbSucceeded(UI\Form $form, $values){
-        libxml_use_internal_errors(true);
-        $doc = new \DOMDocument();
-
-        set_error_handler([$this, "warning_handler"], E_WARNING);
-
-            $doc->loadHTML(file_get_contents($values->url));
-        restore_error_handler();
-//extrahování meta tagů
-        $xpath = new \DOMXPath($doc);
-        $query = '//*/meta[starts-with(@property, \'og:\')]';
-//        $qry = '//*/img[@data-icy-veins]';
-//        $div = $xpath->query($qry);
-//        foreach ($div as $divs){
-//            Debugger::barDump($divs->getAttribute('src'));
+//    public function thumbSucceeded(UI\Form $form, $values){
+//        libxml_use_internal_errors(true);
+//        $doc = new \DOMDocument();
+//
+//        set_error_handler([$this, "warning_handler"], E_WARNING);
+//
+//            $doc->loadHTML(file_get_contents($values->url));
+//        restore_error_handler();
+////extrahování meta tagů
+//        $xpath = new \DOMXPath($doc);
+//        $query = '//*/meta[starts-with(@property, \'og:\')]';
+////        $qry = '//*/img[@data-icy-veins]';
+////        $div = $xpath->query($qry);
+////        foreach ($div as $divs){
+////            Debugger::barDump($divs->getAttribute('src'));
+////        }
+//
+//
+//        $metas = $xpath->query($query);
+//        foreach ($metas as $meta) {
+//            $property = $meta->getAttribute('property');
+//            $content = $meta->getAttribute('content');
+//            if($property === "og:image"){
+//                Debugger::barDump(getimagesize($content));
+//                //ziská výšku a čířku a přetransformuje je aby měla šířku 750
+//                $size = getimagesize($content);
+//                $resizeValue = $size[0] / 650 ;
+//                $width  = $size[0] / $resizeValue;
+//                $height = $size[1] / $resizeValue;
+//                //vložení do tabulky
+//                $this->database->table('odkaz')->insert([
+//                    'imgUrl' => $content,
+//                    'width' => $width,
+//                    'height' => $height
+//                ]);
+//                $this->flashMessage("hotovo", "success");
+//                $this->redirect("this");
+//            }
+//
 //        }
-
-
-        $metas = $xpath->query($query);
-        foreach ($metas as $meta) {
-            $property = $meta->getAttribute('property');
-            $content = $meta->getAttribute('content');
-            if($property === "og:image"){
-                Debugger::barDump(getimagesize($content));
-                //ziská výšku a čířku a přetransformuje je aby měla šířku 750
-                $size = getimagesize($content);
-                $resizeValue = $size[0] / 650 ;
-                $width  = $size[0] / $resizeValue;
-                $height = $size[1] / $resizeValue;
-                //vložení do tabulky
-                $this->database->table('odkaz')->insert([
-                    'imgUrl' => $content,
-                    'width' => $width,
-                    'height' => $height
-                ]);
-                $this->flashMessage("hotovo", "success");
-                $this->redirect("this");
-            }
-
-        }
-
-        $this->flashMessage("url nema thumbnail", "warning");
-        $this->redirect("this");
-
-
-    }
+//
+//        $this->flashMessage("url nema thumbnail", "warning");
+//        $this->redirect("this");
+//
+//
+//    }
 
 
 
